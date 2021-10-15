f59774f41c0b08b72de6834125261e7ebab836b8
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-10-15 15:56:28 +0300

    done

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -13,17 +13,35 @@ menu = [
         "kind": ["Coffee", "Tea", "Unicorn Tears"]
     },
 ]
-print("**************************************")
-print("**    Welcome to the Snakes Cafe!   **")
-print("**    Please see our menu below.    **")
+print("**************")
+print("*    Welcome to the Snakes Cafe!   *")
+print("*    Please see our menu below.    *")
 print("**")
-
-print("**************************************")
+print('* To quit at any time, type "quit" *')
 for i in menu:
     print(i["type"])
     print("----------")
-    for n in i["kind"]:
-        print(n)
-print("**************************************")
-print("** What would you like to order? **")
-print("**************************************")
+    for y in i["kind"]:
+        print(y)
+        print()
+print("**************")
+
+
+if __name__ == "__main__":
+    order_counter = 0
+    orders = []
+    x = ''
+    while x != "quit":
+        x = input(f"* What would you like to order? *>")
+        if x != 'quit':
+            orders.append(x)
+            order_counter += 1
+        elif x == 'quit':
+            print("**************")
+        print(
+            f"* {order_counter} order of {x} have been added to your meal *")
+        print("You can end your order by enter quit")
+        print("This is The List of your oreder..")
+        for i in range(len(orders)):
+            print(orders[i], sep="\n")
+            # print("**************")
